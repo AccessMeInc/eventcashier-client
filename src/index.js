9df01c184fdbcea3948df3103d80574b4b26133c
Tobias Herrmann <tobias@twinfive.com>
2024-04-28 20:57:06 +0200

    BACKEND_SERVER_URL injection added

--- src/index.js
+++ src/index.js
@@ -14,11 +14,15 @@ injectGlobal({
   body: {
     height: "100%",
     width: "100%",
-    background: "#E3E8EE"
+      background: "#f3e5f5"
   }
 });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Obtain the BACKEND_SERVER_URL from environment variables
+const backendUrl = process.env.REACT_APP_BACKEND_SERVER_URL;
+
+// Pass the backendUrl as a prop to the App component
+ReactDOM.render(<App backendUrl={backendUrl} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
